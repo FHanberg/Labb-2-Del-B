d216f33599e4a9e39443364b12e5e1c22d94de69
Hedquist <vilhelm.hedquist@hotmail.com>
2019-12-05 15:00:09 +0100

    Added CarWorld, ICarworld, CarWorldFactory

---
+++ src/carmodel/CarWorld.java
@@ -0,0 +1,101 @@
+package carmodel;
+
+import java.util.ArrayList;
+
+public class CarWorld implements ICarWorld {
+    ArrayList<Car> cars = new ArrayList<>();
+
+    public CarWorld(String[] inputValues) {
+        for(String s : inputValues){
+            String[] splits = s.split(" ");
+            try {
+                addCar(splits[0], Double.parseDouble(splits[1]), Double.parseDouble(splits[2]));
+            } catch (NumberFormatException e) {}
+        }
+    }
+
+    void addCar(String carClassName, double xPos, double yPos) {
+        Car newCar = null;
+        switch (carClassName) {
+            case "Volvo":
+                newCar = new Volvo240(xPos, yPos);
+                break;
+            case "Saab":
+                newCar = new Saab95(xPos, yPos);
+                break;
+            case "Scania":
+                newCar = new Scania(xPos, yPos);
+                break;
+            default:
+                System.out.println("No such car available: " + carClassName);
+                break;
+        }
+        if(newCar != null) cars.add(newCar);
+    }
+
+    @Override
+    public void gas(int amount) {
+        double gas = ((double) amount) / 100;
+        for (Car car : cars) {
+            car.gas(gas);
+        }
+    }
+
+    @Override
+    public void brake(int amount){
+        double brake = amount / 100.0;
+        for(Car car : cars){
+            car.brake(brake);
+        }
+    }
+
+    @Override
+    public void turboOn(){
+        for(Car car : cars){
+            if(car instanceof ITurbo){
+                ((ITurbo) car).setTurboOn();
+            }
+        }
+    }
+
+    @Override
+    public void turboOff(){
+        for(Car car : cars){
+            if(car instanceof ITurbo){
+                ((ITurbo) car).setTurboOff();
+            }
+        }
+    }
+
+    @Override
+    public void liftBeds(){
+        for(Car car : cars){
+            if(car instanceof Flatbed){
+                ((Flatbed) car).raiseBed();
+            }
+        }
+    }
+
+    @Override
+    public void lowerBeds(){
+        for (Car car : cars){
+            if(car instanceof Flatbed){
+                ((Flatbed) car).lowerBed();
+            }
+        }
+    }
+
+    @Override
+    public void startCars(){
+        for(Car car : cars){
+            car.startEngine();
+        }
+    }
+
+    @Override
+    public void stopCars(){
+        for(Car car : cars){
+            car.stopEngine();
+        }
+    }
+}
---
+++ src/carmodel/ICarWorld.java
@@ -0,0 +1,12 @@
+package carmodel;
+
+public interface ICarWorld {
+    void gas(int amount);
+    void brake(int amount);
+    void turboOn();
+    void turboOff();
+    void liftBeds();
+    void lowerBeds();
+    void startCars();
+    void stopCars();
+}
